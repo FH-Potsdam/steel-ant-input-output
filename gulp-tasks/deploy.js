54ba4efa65d55ba23e6d8f60609410efab9d89b1
fabiantheblind <icke@fabianmoronzirfas.me>
2015-11-16 13:53:08 +0100

    update local server

--- gulp-tasks/deploy.js
+++ gulp-tasks/deploy.js
@@ -1,5 +1,11 @@
+var git = require('gulp-git');
+var os      = require('os');
+var package = require('../package.json');
+var path    = require('path');
 var ghPages = require('gulp-gh-pages');
-module.exports = function(gulp){
-  return gulp.src('./**/*')
+
+module.exports = function(gulp) {
+   return gulp.src('./../**/*')
     .pipe(ghPages());
+
 };
--- gulpfile.js
+++ gulpfile.js
@@ -2,17 +2,24 @@
 // require('require-dir')('./gulp-tasks');
 var gulp = require('gulp');
 var test = require('./gulp-tasks/test');
-var deploy = require('./gulp-tasks/deploy');
+// var deploy = require('./gulp-tasks/deploy');
+var ghPages = require('gulp-gh-pages');
+
 // Load all tasks from folder `gulp-tasks`
 gulp.task('test', function() {
   // place code for your default task here
 test(gulp);
 });
 
-gulp.task('deploy', function() {
-  // place code for your default task here
-deploy(gulp);
+gulp.task('deploy',function(){
+   return gulp.src('./**/*')
+    .pipe(ghPages());
 });
+
+// gulp.task('deploy', function() {
+//   // place code for your default task here
+// deploy(gulp);
+// });
 
 
 // use it!
