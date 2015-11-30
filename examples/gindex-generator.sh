eb71ff551e557434b9324e32c56cb054ddab5653
fabiantheblind <icke@fabianmoronzirfas.me>
2015-11-30 15:58:03 +0100

    added generator clear

--- examples/gindex-generator.sh
+++ examples/gindex-generator.sh
@@ -3,6 +3,7 @@
 for D in *; do [ -d "${1}" ] addIndex ${1} && done
 
 function addIndex(){
+  echo "" > ${1}/index.md
   echo "---" >> ${1}/index.md
   echo "layout: example" >> ${1}/index.md
   echo "title: ${1}" >> ${1}/index.md
